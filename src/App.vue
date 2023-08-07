5dadb2d32cddcffb9b5c9c24ef6eb325592f02a8
GPegov <askpegov@gmail.com>
2023-08-07 21:30:07 +0500

    Add project code

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="app">
+        <post-form/> 
+        <post-list :posts="posts"/>
+
+        
+        
+    </div>
+   
+</template>   
+
+
+<script>
+import PostForm from '@/components/PostForm';
+import PostList from '@/components/PostList';
+
+export default {
+    components: {
+        PostList, PostForm
+    },
+    data() {
+        return {
+            posts: [
+                {id:1, title: 'JavaScript', body: 'Описание Поста 1'},
+                {id:2, title: 'JavaScript', body: 'Описание Поста 2'},
+                {id:3, title: 'JavaScript', body: 'Описание Поста 3'},  
+            ],
+            title:"",
+            body:"",
+        }
+    },
+    methods: {
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost)
+            this.title = ''
+            this.body = ''
+        },
+    }
+
+}
+</script>
+
+<style>
+*{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+.app {
+    padding: 20px;
+}
+
+
+</style>
