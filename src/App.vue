af4ad1cb9109e8f3fe2fd38330dc444c61c18043
GPegov <askpegov@gmail.com>
2023-08-13 15:48:21 +0500

    adding server commands

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="app">
-        <my-dialog :show="true">
+        <h1>Страница с постами</h1>
+        <my-button
+        @click="fetchPosts"
+        >
+        Получить посты
+        </my-button>
+        <input type="text" v-model.trim='modificatorValue'>
+        <my-button 
+        @click="showDialog"
+        style="margin: 15px 0;"   
+        >
+        Создать пост
+        </my-button>
+        <my-dialog v-model:show="dialogVisible">
             <post-form
         @create="createPost"
         /> 
@@ -18,6 +31,7 @@
 <script>
 import PostForm from '@/components/PostForm';
 import PostList from '@/components/PostList';
+import axios from 'axios'
 
 export default {
     components: {
@@ -30,14 +44,28 @@ export default {
                 {id:2, title: 'JavaScript', body: 'Описание Поста 2'},
                 {id:3, title: 'JavaScript', body: 'Описание Поста 3'},  
             ],
+            dialogVisible: false,
+            modificatorValue:''
         }
     },
     methods: {
         createPost(post) {
             this.posts.push(post)
+            this.dialogVisible = false
         },
         removePost(post){
             this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showDialog() {
+            this.dialogVisible = true
+        },
+        async fetchPosts() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                console.log(responce)
+            } catch (e) {
+                alert('Ошибка')
+            }
         }
     }
 
